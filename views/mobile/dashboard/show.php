e57f63f5210e7b3e0f97c20e21ca16b2821f681d
Robert Fine <thefinestrobert@gmail.com>
2026-01-04 08:01:27 -0500

    fix: remove non-functional image lightbox and stray X button from mobile dashboard

--- views/mobile/dashboard/show.php
+++ views/mobile/dashboard/show.php
@@ -3,23 +3,15 @@
 // MOBILE "COMMAND CONSOLE" VIEW
 // =================================================================
 ?>
-<!-- Image Lightbox Structure -->
-<div id="avatar-lightbox" class="image-lightbox">
-    <span class="lightbox-close">&times;</span>
-    <img class="lightbox-content" id="lightbox-img">
-</div>
-
 <div class="mobile-content">
     <div class="player-hub">
-        <a href="#" id="avatar-trigger">
-            <?php if ($user->profile_picture_url): ?>
-                <img src="/serve/avatar/<?= htmlspecialchars($user->profile_picture_url) ?>" alt="Avatar" class="player-avatar large">
-            <?php else: ?>
-                <svg class="player-avatar large" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
-                </svg>
-            <?php endif; ?>
-        </a>
+        <?php if ($user->profile_picture_url): ?>
+            <img src="/serve/avatar/<?= htmlspecialchars($user->profile_picture_url) ?>" alt="Avatar" class="player-avatar large">
+        <?php else: ?>
+            <svg class="player-avatar large" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
+            </svg>
+        <?php endif; ?>
         <h2 class="player-name"><?= htmlspecialchars($user->characterName) ?></h2>
         <?php if ($user->alliance_id): ?>
             <a href="/alliance/profile/<?= $user->alliance_id ?>" class="player-alliance-link">View Alliance</a>
