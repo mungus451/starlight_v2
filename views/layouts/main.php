c4be16b0a4135dcdb7b81fcc922ed78a1283e72a
Robert Fine <thefinestrobert@gmail.com>
2026-01-03 20:38:16 -0500

    cache busting fix #1

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Operation Starlight CSS -->
-    <link rel="stylesheet" href="/css/starlight.css?v=1.1">
+    <link rel="stylesheet" href="/css/starlight.css?v=<?= time() ?>">
 </head>
 <body>
 
